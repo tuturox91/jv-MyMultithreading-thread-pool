b191fb4b03bb661e151e3096f3fa6c65256da589
Igor Bukur <tuturox91@gmail.com>
2022-12-31 15:19:04 +0200

    Finish

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,8 +12,27 @@ import org.apache.logging.log4j.Logger;
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
+
+
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        MyThread task = new MyThread();
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+
+        for(int i = 0; i<20; i++) {
+            futures.add(executorService.submit(task));
+        }
+        executorService.shutdown();
+
+        for(Future future : futures) {
+            try {
+                logger.info(future.get());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException("Some exception was encountered!", e);
+            }
+        }
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,16 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int sleepTime = (new Random().nextInt(5)+ 1) * 100;
+        Thread.sleep(sleepTime);
+        return "Task duration was " +  sleepTime + "ms, execution finished at " + LocalTime.now();
     }
 }
